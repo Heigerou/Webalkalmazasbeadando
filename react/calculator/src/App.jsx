6f2dec5cb553c7376c3e1969a108a080e8f11e7c
PecsySzabolcs <136331697+Heigerou@users.noreply.github.com>
2025-04-18 19:25:02 +0200

    React
    Calculator & Tictactoe

---
+++ react/calculator/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from './Calculator';
+import './Calculator.css';
+
+function App() {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
